1ce708314f8325a19d643fc7726d1d365ab5efad
enesseval <enesseval@outlook.com>
2025-04-09 18:18:51 +0300

    update set cookie

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -145,6 +145,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
          secure: true, // Production'da HTTPS üzerinden çalışacağı için true kalmalı
          domain: "yazar.vercel.app",
          path: "/",
+         sameSite: "none",
          maxAge: 60 * 60 * 1000, // 1 saat
       });
 
