eae3cd15a66b422f18882489e9ec986a8d82bd5e
enesseval <enesseval@outlook.com>
2025-04-09 18:28:30 +0300

    6

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -143,7 +143,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
          httpOnly: true,
          // domain: "yazar.vercel.app", // Kaldırıldı
          secure: true, // Production'da HTTPS üzerinden çalışacağı için true kalmalı
-         domain: "yazar.vercel.app",
+
          path: "/",
          sameSite: "none",
          maxAge: 60 * 60 * 1000, // 1 saat
